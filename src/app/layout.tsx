35e6849b5ea1344876eaddf7b70d478ab03757e8
FarhanAbelRantisi <farhanrantisi55@gmail.com>
2026-07-18 15:37:32 +0700

    fix: logo roemah bimbel on link

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,12 +6,29 @@ import SessionProvider from "@/components/SessionProvider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
+  metadataBase: new URL("https://roemahbimbel.com"),
   title: "Roemah Bimbel",
-  description: "Platform simulasi ujian SKD",
+  description: "Platform simulasi ujian SKD, CPNS, dan Kedinasan.",
   icons: {
-	icon: "/favico.ico",
-	apple: "/favico.ico",
-	},
+    icon: "/favico.ico",
+    apple: "/favico.ico",
+  },
+  openGraph: {
+    title: "Roemah Bimbel",
+    description: "Platform simulasi ujian SKD, CPNS, dan Kedinasan.",
+    url: "https://roemahbimbel.com",
+    siteName: "Roemah Bimbel",
+    images: [
+      {
+        url: "/images/logo_roemahbimbel.png",
+        width: 800,
+        height: 800,
+        alt: "Logo Roemah Bimbel",
+      }
+    ],
+    locale: "id_ID",
+    type: "website",
+  }
 };
 
 export default function RootLayout({
